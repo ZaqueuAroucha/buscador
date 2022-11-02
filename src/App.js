51276565d7bfff31e74401bf2aaddd6943d52ab7
zaqueu aroucha da silva <zaqueuaroucha@gmail.com>
2022-11-02 15:56:23 -0300

    Subindo projeto Buscador de CEP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="constainerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep..."
+        />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 79003222</h2>
+
+        <span>Rua Teste algum</span>
+        <span>Complemento: Algum complemento</span>
+        <span>Vila Rosa</span>
+        <span>Campo Grande - MS</span>
+
+      </main>
+
     </div>
   );
 }
